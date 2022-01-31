3f9729ee4e140feec29dcc2d13f3317a9b2ecbbd
tanmyavishvakarma <tanmya2000@gmail.com>
2022-02-01 01:17:07 +0530

    clutter handled v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express')
 const app=express()
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
-const routes=require('./routes/routes')
+const authRoutes=require('./routes/authRoutes')
 const cors=require('cors')
 const bodyParser=require("body-parser")
 const session=require("express-session") 
@@ -37,7 +37,7 @@ app.use(session({
 app.use(cookieparser("secret"))
 
 app.use(express.json())
-app.use('/',routes)
+app.use('/',authRoutes)
 const PORT=process.env.PORT||5000
 app.listen(PORT,()=>{
   console.log("server is running succesfully")
@@ -45,30 +45,3 @@ app.listen(PORT,()=>{
 
 module.exports = app;
 
-
-// router.put('/verifyuser',async (req,res)=>{
-//   User.findByIdAndUpdate({email: req.params.email},{verified:true})
-//   .then(data => {
-      
-//       if (!data) {
-//         res.status(404).send({
-//           message: `Update Unsuccessful`
-//         });
-//       } else {
-//           // const token = generateJwtToken(savedUser._id, savedUser.role);
-//           // const { _id, name, email,mobile, role } = savedUser;
-//           // res.status(200).json({
-//           //   token,
-//           //   user: { _id, name, email,mobile, role },
-//           // });
-//           res.status(200).send("Update Succesful")
-//       }   
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error in updating" 
-//       });
-//     }); 
- 
-
-// });
